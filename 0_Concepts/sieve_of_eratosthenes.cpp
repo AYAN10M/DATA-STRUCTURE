bb5c5056fec62434be891435f6e5d091daa4d537
AYAN10M <gopalchandrahaldar1997@gmail.com>
2025-08-19 11:24:47 +0530

    few more things to do

--- 0_Concepts/sieve_of_eratosthenes.cpp
+++ 0_Concepts/sieve_of_eratosthenes.cpp
@@ -1,4 +1,55 @@
 /*
     Resource Link : https://www.geeksforgeeks.org/dsa/sieve-of-eratosthenes/
+
+    Step 1:
+    Initialize a Boolean array prime[0..n] and set all entries to true, except for 0 and 1 (which are not primes).
+
+    Step 2:
+    Start from 2, the smallest prime number.
+
+        *For each number p from 2 up to √n:
+
+        * If p is marked as prime(true):
+            Mark all multiples of p as not prime(false), starting from p * p (since smaller multiples have already been marked by smaller primes).
+
+    Step 3:
+    After the loop ends, all the remaining true entries in prime represent prime numbers.
 */
 
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> sieve(int n)
+{
+    vector<bool> prime(n + 1, true);
+    for (int p = 2; p * p <= n; p++)
+    {
+        if (prime[p] == true)
+        {
+            // marking as false
+            for (int i = p * p; i <= n; i += p)
+                prime[i] = false;
+        }
+    }
+
+    vector<int> res;
+    for (int p = 2; p <= n; p++)
+    {
+        if (prime[p])
+        {
+            res.push_back(p);
+        }
+    }
+    return res;
+}
+
+int main()
+{
+    int n = 35;
+    vector<int> res = sieve(n);
+    for (auto ele : res)
+    {
+        cout << ele << ' ';
+    }
+    return 0;
+}
--- 14_Math/t_primes_codeforces.cpp
+++ 14_Math/t_primes_codeforces.cpp
@@ -1,14 +1,14 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-bool isPrime(int n)
+bool isPrime(long long n)
 {
     if (n < 2)
     {
         return false;
     }
 
-    for (int i = 2; i * i <= n; i++)
+    for (long long i = 2; i * i <= n; i++)
     {
         if (n % i == 0)
         {
@@ -20,19 +20,17 @@ bool isPrime(int n)
 
 int main()
 {
-    int n;
+    long long n;
     cin >> n;
-    vector<int> arr;
-    for (int i = 0; i < n; i++)
+    vector<long long> arr(n);
+    for (auto i = 0; i < n; i++)
     {
-        int input;
-        cin >> input;
-        arr.push_back(input);
+        cin >> arr[i];
     }
 
     for (auto i : arr)
     {
-        int x = sqrt(i);
+        long long x = sqrtl(i);
         if (x * x != i)
         {
             cout << "NO" << endl;
