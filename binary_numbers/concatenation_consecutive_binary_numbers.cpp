9aa6c51c2d421176cf1430d180a7b2888e745b1a
AYAN HALDAR <119616417+AYAN10M@users.noreply.github.com>
2026-03-01 10:43:57 +0530

    done :)

--- binary_numbers/concatenation_consecutive_binary_numbers.cpp
+++ binary_numbers/concatenation_consecutive_binary_numbers.cpp
@@ -8,7 +8,7 @@ public:
     {
         if(n == 0)
         {
-            return "";
+            return "0";
         }
 
         string result = "";
@@ -19,11 +19,40 @@ public:
 
             result.push_back(temp + '0');
         }
+        reverse(result.begin(), result.end());
 
         return result;
     }
 
+    int toInteger(string binary)
+    {
+        long long num = 0;
+
+        for(int i = 0; i < binary.size(); i++)
+        {
+            num = num * 2 + (binary[i] - '0');
+        }
+
+        return num;
+    }
+
     int concatenatedBinary(int n) 
     {
+        const int MOD = 1e9 + 7;
+        string temp = "";
+
+        for(int i = 1; i <= n; i++)
+        {
+            temp += toBinary(i);
+        }
+
+        long long result = 0;
+
+        for(int i = 0; i < temp.size(); i++)
+        {
+            result = (result * 2 + (temp[i] - '0')) % MOD;
+        }
+
+        return result;
     }
 };
